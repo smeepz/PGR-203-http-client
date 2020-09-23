ee5dcb0d789f77ccd32999f2752f3a21aee61938
smeepz <stianlw@gmail.com>
2020-09-23 11:45:30 +0200

    added comments

--- http-client-master/src/main/java/no/kristiania/http/HttpServer.java
+++ http-client-master/src/main/java/no/kristiania/http/HttpServer.java
@@ -7,29 +7,39 @@ import java.net.Socket;
 public class HttpServer {
 
     public HttpServer(int port) throws IOException {
+        //Open a entry point to our program for network clients
         ServerSocket serverSocket = new ServerSocket(port);
 
-        new Thread(() -> {
+        //New Threads executes the code in a separate "thread", that is in parallel
+        new Thread(() -> { //Annonymous function with code that will be executed in parallel
             while (true) {
                 try {
-                    handleRequest(serverSocket.accept());
+                    //Accept waits for a client to try to connect - blocks
+                    Socket clientSocket = serverSocket.accept();
+                    handleRequest(clientSocket);
                 } catch (IOException e) {
+                    //If something went wrong - print the exception and try again
                     e.printStackTrace();
                 }
             }
-        }).start();
+        }).start(); //Start the threads, so the code inside executes without blocking the current thread
     }
 
+    //This code will be executed for each client
     private void handleRequest(Socket clientSocket) throws IOException {
         String requestLine = HttpClient.readLine(clientSocket);
         System.out.println(requestLine);
+        //Example: GET /echo?body=Hello HTTP/1.1
 
         String requestTarget = requestLine.split(" ")[1];
+        //Example: "/echo?body=Hello"
         String statusCode = "200";
 
         int questionPos = requestTarget.indexOf('?');
         if (questionPos != -1) {
             String queryString = requestTarget.substring(questionPos+1);
+            //body = hello
+
             int equalPos = queryString.indexOf('=');
             String parameterName = queryString.substring(0, equalPos);
             String parameterValue = queryString.substring(equalPos+1);
@@ -42,6 +52,7 @@ public class HttpServer {
                 "\r\n" +
                 "Hello <strong>World</strong>!";
 
+        //Write the response back to the client
         clientSocket.getOutputStream().write(response.getBytes());
     }
 
